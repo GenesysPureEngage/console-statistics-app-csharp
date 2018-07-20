6933b6e6302e61a5d08e05e1b5663484ea51835c
Jim Crespino <jim.crespino@genesys.copm>
2018-07-20 13:08:19 -0500

    Fixed a copy/paste naming issue

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("console-agent-app-csharp")]
+[assembly: AssemblyTitle("console-statistics-app-csharp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
